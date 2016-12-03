c076bf0abc29d8b7b2c7f8a71e77b6b5d16e0d6e
Richard Jones <r1chardj0n3s@gmail.com>
2016-12-03 11:13:19 +1100

    add hand recipe

--- main/java/net/mechanicalcat/pycode/init/ModCrafting.java
+++ main/java/net/mechanicalcat/pycode/init/ModCrafting.java
@@ -3,6 +3,7 @@ package net.mechanicalcat.pycode.init;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.EnumDyeColor;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -24,6 +25,16 @@ public class ModCrafting {
                 " RY",
                 "S  ",
                 'S', Items.STICK,
+                'L', new ItemStack(Items.DYE, 1, EnumDyeColor.BLUE.getDyeDamage()),
+                'Y', new ItemStack(Items.DYE, 1, EnumDyeColor.YELLOW.getDyeDamage()),
+                'R', Items.REDSTONE
+        );
+        GameRegistry.addShapedRecipe(
+                new ItemStack(ModItems.python_hand),
+                " L ",
+                "WRY",
+                " W ",
+                'W', Blocks.WOOL,
                 'L', new ItemStack(Items.DYE, 1, EnumDyeColor.BLUE.getDyeDamage()),
                 'Y', new ItemStack(Items.DYE, 1, EnumDyeColor.YELLOW.getDyeDamage()),
                 'R', Items.REDSTONE
